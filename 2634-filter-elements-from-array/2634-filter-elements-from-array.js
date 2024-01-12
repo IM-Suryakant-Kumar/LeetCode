02b1c375878ef979628ce669c7ab434412a807e1
Suryakant Kumar <66691162+IM-Suryakant-Kumar@users.noreply.github.com>
2024-01-12 11:19:09 +0530

    Time: 50 ms (69.51%), Space: 41.8 MB (68.14%) - LeetHub

--- 2634-filter-elements-from-array/2634-filter-elements-from-array.js
+++ 2634-filter-elements-from-array/2634-filter-elements-from-array.js
@@ -5,9 +5,8 @@
  */
 var filter = function(arr, fn) {
     const resArr = [];
-    let i = 0;
-    for(let val of arr) {
-        if(fn(val, i++)) resArr.push(val)
+    for(let i = 0; i < arr.length; i++ ) {
+        if(fn(arr[i], i)) {resArr.push(arr[i])}
     }
     return resArr;
 };
