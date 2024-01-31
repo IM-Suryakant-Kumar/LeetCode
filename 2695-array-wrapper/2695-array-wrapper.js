36346f2790ba921f3f9ae914dbc4fd214cb84310
Suryakant Kumar <66691162+IM-Suryakant-Kumar@users.noreply.github.com>
2024-01-31 08:47:17 +0530

    Time: 53 ms (60.75%), Space: 50.1 MB (13.12%) - LeetHub

--- 2695-array-wrapper/2695-array-wrapper.js
+++ 2695-array-wrapper/2695-array-wrapper.js
@@ -2,22 +2,22 @@
  * @param {number[]} nums
  * @return {void}
  */
-var ArrayWrapper = function(nums) {
-    this.nums = nums 
+var ArrayWrapper = function (nums) {
+    this.nums = nums
 };
 
 /**
  * @return {number}
  */
-ArrayWrapper.prototype.valueOf = function() {
+ArrayWrapper.prototype.valueOf = function () {
     return this.nums.reduce((n, a) => n + a, 0)
 }
 
 /**
  * @return {string}
  */
-ArrayWrapper.prototype.toString = function() {
-    return `[${this.nums}]`;
+ArrayWrapper.prototype.toString = function () {
+    return `[${String(this.nums)}]`;
 }
 
 /**
