be0aa26cec88d30c9aa64b4cf46a5765258c897a
Suryakant Kumar <66691162+IM-Suryakant-Kumar@users.noreply.github.com>
2024-02-01 06:05:45 +0530

    Time: 66 ms (10%), Space: 48.9 MB (8.47%) - LeetHub

--- 2704-to-be-or-not-to-be/2704-to-be-or-not-to-be.js
+++ 2704-to-be-or-not-to-be/2704-to-be-or-not-to-be.js
@@ -4,14 +4,14 @@
  */
 var expect = function(val) {
     return {
-        toBe: (val2) => {
-            if(val === val2) return true
-            else throw new Error("Not Equal")
+        toBe: val2 => {
+            if(val === val2) return true;
+            throw new Error("Not Equal")
         },
-        notToBe: (val2) => {
-            if(val !== val2) return true
-            else throw new Error("Equal")
-        }
+        notToBe: val2 => {
+            if(val !== val2) return true;
+            throw new Error("Equal")
+        },
     }
 };
 
