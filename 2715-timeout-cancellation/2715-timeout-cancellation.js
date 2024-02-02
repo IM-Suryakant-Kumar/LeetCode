fe0da1bf21ec3bc30ccb90eebb2412c75f70c4b8
Suryakant Kumar <66691162+IM-Suryakant-Kumar@users.noreply.github.com>
2024-02-03 05:12:35 +0530

    Time: 68 ms (37.65%), Space: 48.7 MB (22.09%) - LeetHub

--- 2715-timeout-cancellation/2715-timeout-cancellation.js
+++ 2715-timeout-cancellation/2715-timeout-cancellation.js
@@ -6,10 +6,7 @@
  */
 var cancellable = function(fn, args, t) {
     const timeoutId = setTimeout(() => fn(...args), t)
-    
-    return function cancelFn () {
-        clearTimeout(timeoutId)
-    }
+    return () => clearTimeout(timeoutId)
 };
 
 /**
