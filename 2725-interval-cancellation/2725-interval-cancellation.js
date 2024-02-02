ff4c488e4eb493d85b9e4756a32f9f0e9ec01f53
Suryakant Kumar <66691162+IM-Suryakant-Kumar@users.noreply.github.com>
2024-02-03 05:26:44 +0530

    Time: 66 ms (51.81%), Space: 49.3 MB (16.49%) - LeetHub

--- 2725-interval-cancellation/2725-interval-cancellation.js
+++ 2725-interval-cancellation/2725-interval-cancellation.js
@@ -6,7 +6,7 @@
  */
 var cancellable = function(fn, args, t) {
     fn(...args)
-    const intervalId = setInterval(() => {fn(...args)}, t);
+    const intervalId = setInterval(() => fn(...args), t);
     return () => clearInterval(intervalId)
 };
 
