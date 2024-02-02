dab3835f838cbb2fdab563227a6935b8a4545d31
Suryakant Kumar <66691162+IM-Suryakant-Kumar@users.noreply.github.com>
2024-02-02 06:24:14 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2623-memoize/2623-memoize.js
+++ 2623-memoize/2623-memoize.js
@@ -3,19 +3,18 @@
  * @return {Function}
  */
 function memoize(fn) {
-    const cache = {}
-    
-    return function(...args) {
-        const key = JSON.stringify(args)
+    const cache = {};
+
+    return function (...args) {
+        const key = JSON.stringify(args);
+
+        if (cache[key]) {
+            return cache[key];
+        } 
         
-        if(key in cache) {
-            return cache[key]
-        }
-        
-        cache[key] = fn(...args)
-        
-        return cache[key]
+        return cache[key] = fn(...args);
     }
+
 }
 
 
