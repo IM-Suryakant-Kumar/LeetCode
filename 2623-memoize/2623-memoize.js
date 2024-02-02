e4ac4212f1040ce7193e8bc83d6fccfd3487edb4
Suryakant Kumar <66691162+IM-Suryakant-Kumar@users.noreply.github.com>
2024-02-02 06:25:27 +0530

    Time: 325 ms (23.79%), Space: 84.7 MB (99.79%) - LeetHub

--- 2623-memoize/2623-memoize.js
+++ 2623-memoize/2623-memoize.js
@@ -8,7 +8,7 @@ function memoize(fn) {
     return function (...args) {
         const key = JSON.stringify(args);
 
-        if (cache[key]) {
+        if (key in cache) {
             return cache[key];
         } 
         
