cd0ff10e880beb4817a46a1f3d1b6e3f390fa01a
Suryakant Kumar <66691162+IM-Suryakant-Kumar@users.noreply.github.com>
2024-10-02 07:08:41 +0530

    Time: 55 ms (82.23%), Space: 50.9 MB (42.88%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    const map = new Map();
+
+    for(let i = 0; i < nums.length; i++) {
+        const num1 = nums[i];
+        const num2 = target - num1;
+
+        if(map.has(num2)) {
+            return [i, map.get(num2)]
+        }
+        map.set(num1, i);
+    }
+};
