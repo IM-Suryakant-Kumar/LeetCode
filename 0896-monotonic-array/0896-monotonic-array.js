8105747e784cc6131dfe63cf2aed1dd04d9d0bd5
Suryakant Kumar <66691162+IM-Suryakant-Kumar@users.noreply.github.com>
2024-10-20 19:22:21 +0530

    Time: 1 ms (100%), Space: 61.4 MB (37.1%) - LeetHub

--- 0896-monotonic-array/0896-monotonic-array.js
+++ 0896-monotonic-array/0896-monotonic-array.js
@@ -6,18 +6,18 @@ var isMonotonic = function(nums) {
     const first = nums[0];
     const last = nums[nums.length - 1];
     if(first === last) {
-        for(let i = 0; i < nums.length; i++) {
-            if(nums[i] !== nums[i + 1]) return false;
+        for(let i = 1; i < nums.length; i++) {
+            if(nums[i - 1] !== nums[i]) return false;
         }
     } else if(first < last) {
         // non-decreasing
-        for(let i = 0; i < nums.length; i++) {
-            if(nums[i] > nums[i + 1]) return false;
+        for(let i = 1; i < nums.length; i++) {
+            if(nums[i - 1] > nums[i]) return false;
         }
     } else {
         // non-increasing
-        for(let i = 0; i < nums.length; i++) {
-            if(nums[i] < nums[i + 1]) return false;
+        for(let i = 1; i < nums.length; i++) {
+            if(nums[i - 1] < nums[i]) return false;
         }
     }
     return true
