f80a2218e1ae07543e7dad34842dedbccba706d8
Suryakant Kumar <66691162+IM-Suryakant-Kumar@users.noreply.github.com>
2024-10-26 08:38:10 +0530

    Time: 0 ms (100%), Space: 48.7 MB (53.97%) - LeetHub

--- 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.js
+++ 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.js
@@ -5,12 +5,10 @@
  */
 var kthGrammar = function(n, k) {
     if(n === 1) return 0;
-
-    const length = 2 ** (n - 1);
+    
+    const length  = Math.pow(2, (n - 1));
     const mid = length / 2;
-    if(k <= mid) {
-        return kthGrammar(n - 1, k);
-    } else {
-        return 1 - kthGrammar(n - 1, k - mid)
-    }
+    
+    if(k <= mid) return kthGrammar(n - 1, k);
+    else return 1 - kthGrammar(n - 1, k - mid);
 };
