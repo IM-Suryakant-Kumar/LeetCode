018949f3493c9544271f0bbac4100a4ad291aecf
Suryakant Kumar <66691162+IM-Suryakant-Kumar@users.noreply.github.com>
2024-11-26 12:44:52 +0530

    Time: 10 ms (85.14%), Space: 60.3 MB (82.44%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -3,22 +3,6 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    // // Approach 1
-    // nums.sort((a, b) => a - b);
-    // for(let i = 1; i < nums.length; i++) {
-    //     if(nums[i - 1] === nums[i]) return true;
-    // }
-    // return false;
-
-    // // Approach 2
-    // const track = {};
-    // for(let i = 0; i < nums.length; i++) {
-    //     if(nums[i] in track) return true;
-    //     track[nums[i]] = i;
-    // }
-    // return false;
-
-    // Approach 3
     const track = new Set();
     for(let i = 0; i < nums.length; i++) {
         if(track.has(nums[i])) return true;
