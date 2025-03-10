a50263317558d57c6f32d30b4d5e64f68e12c857
Suryakant Kumar <66691162+IM-Suryakant-Kumar@users.noreply.github.com>
2025-03-10 08:38:16 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0169-majority-element/0169-majority-element.js
+++ 0169-majority-element/0169-majority-element.js
@@ -3,21 +3,33 @@
  * @return {number}
  */
 var majorityElement = function(nums) {
-    nums.sort((a, b) => a - b);
-    let freq = 1, ans = nums[0], n = nums.length;
 
-    for(let i = 1; i < n; i++ ) {
+    // approach 1
+    // for(let val of nums) {
+    //     let freq = 0;  
+    //     for(let el of nums) {
+    //         if(val === el) {
+    //             freq++;
+    //         }
+    //     }
+    //     if(freq > nums.length / 2) return val;
+    // }
+
+    // return -1;
+
+    // approach 2
+    nums.sort((a, b) => a - b);
+    let freq = 0, ans = nums[0];
+
+    for(let i = 1; i < nums.length; i++) {
         if(nums[i] === nums[i - 1]) {
             freq++;
         } else {
             freq = 1;
             ans = nums[i];
         }
-
-        if(freq > n / 2) {
-            return ans;
-        }
+        if(freq > nums.length / 2) return ans
     }
-
+    
     return ans;
 };
