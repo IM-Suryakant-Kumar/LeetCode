386b438202a0d00f087185fae263e21aa7d84067
Suryakant Kumar <66691162+IM-Suryakant-Kumar@users.noreply.github.com>
2025-03-14 15:52:00 +0530

    Time: 3 ms (58.65%), Space: 65.3 MB (19.88%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -3,15 +3,12 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    let maxProfit = 0;
-    let l = 0, r = 1;
-    while(r < prices.length) {
-        if(prices[l] < prices[r]) {
-            maxProfit = Math.max(maxProfit, prices[r] - prices[l])
-        } else {
-            l = r;
-        }
-        r++;
+    let buy = prices[0], ans = 0;
+
+    for(let i = 1; i < prices.length; i++) {
+        if(buy < prices[i]) ans = Math.max(ans, prices[i] - buy);
+        else buy = prices[i];
     }
-    return maxProfit;
+
+    return ans;
 };
