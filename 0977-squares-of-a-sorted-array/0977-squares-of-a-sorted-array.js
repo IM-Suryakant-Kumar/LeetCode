dfdc9ddd82b9adac4a7ac572f8420bf28a340fd2
Suryakant Kumar <66691162+IM-Suryakant-Kumar@users.noreply.github.com>
2025-03-25 15:11:53 +0530

    Time: 1 ms (99.52%), Space: 62.8 MB (17.62%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.js
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.js
@@ -3,26 +3,21 @@
  * @return {number[]}
  */
 var sortedSquares = function(nums) {
-    // // Approach 1
-    // const ans = new Array(nums.length).fill(0);
-    // for(let i = 0; i < nums.length; i++) {
-    //     ans[i] = Math.pow(nums[i], 2);
-    // }
-    // return ans.sort((a, b) => a - b)
-
-    // Approach 2
     const ans = new Array(nums.length).fill(0);
-    let leftPointer = 0, rightPointer = nums.length - 1;
+    let l = 0, r = nums.length - 1;
+    
     for(let i = nums.length - 1; i >= 0; i--) {
-        const leftSquared = nums[leftPointer] ** 2;
-        const rightSquared = nums[rightPointer] ** 2;
-        if(leftSquared > rightSquared) {
-            ans[i] = leftSquared;
-            leftPointer++;
+        const leftEle = nums[l] ** 2;
+        const rightEle = nums[r] ** 2;
+
+        if(leftEle > rightEle) {
+            ans[i] = leftEle;
+            l++;
         } else {
-            ans[i] = rightSquared;
-            rightPointer--;
+            ans[i] = rightEle;
+            r--;
         }
     }
+
     return ans;
 };
