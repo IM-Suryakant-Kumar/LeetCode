8bd01f0f42b0dd17e586bd0a52f6fd4bd8b58519
Suryakant Kumar <66691162+IM-Suryakant-Kumar@users.noreply.github.com>
2025-04-01 11:07:12 +0530

    Time: 35 ms (94.22%), Space: 56.3 MB (5.28%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.js
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.js
@@ -4,11 +4,13 @@
  * @return {string}
  */
 var mergeAlternately = function(word1, word2) {
-    let ans = "", i = 0, j = 0;
+    let ans = "";
+    let i = 0, j = 0;
 
     while(i < word1.length || j < word2.length) {
-        ans += (word1[i++] ?? "") + (word2[j++] ?? "");
+        if(word1[i]) ans += word1[i++];
+        if(word2[j]) ans += word2[j++];
     }
-    
+
     return ans;
 };
