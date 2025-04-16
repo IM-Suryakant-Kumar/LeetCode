3b1c607216268bfbdadc2d19b8b02cabec6333d6
Suryakant Kumar <66691162+IM-Suryakant-Kumar@users.noreply.github.com>
2025-04-16 16:24:56 +0530

    Time: 48 ms (32.89%), Space: 55.6 MB (8.35%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.js
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.js
@@ -3,13 +3,11 @@
  * @param {string} word2
  * @return {string}
  */
-var mergeAlternately = function(word1, word2) {
-    let ans = "";
-    let i = 0, j = 0;
+var mergeAlternately = function (word1, word2) {
+    let ans = "", i = 0;
 
-    while(i < word1.length || j < word2.length) {
-        if(word1[i]) ans += word1[i++];
-        if(word2[j]) ans += word2[j++];
+    while (i < word1.length || i < word2.length) {
+        ans += (word1[i] ?? "") + (word2[i++] ?? "");
     }
 
     return ans;
